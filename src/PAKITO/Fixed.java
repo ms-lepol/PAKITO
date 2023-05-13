efe8cc2dff8a0388e493e3c1d3402928e90b4d9e
TriTrisTouiL <tristandsg@gmail.com>
2023-05-13 12:49:37 +0200

    oopsie

--- src/PAKITO/Fixed.java
+++ src/PAKITO/Fixed.java
@@ -7,7 +7,10 @@ abstract class Fixed extends Piece implements Questionnable {
      * cette fonction fait bouger le chasseur sur la case de 
      * la piece fixe et supprime le chasseur de sa case précédente
      * 
-     * Ce bout de code est réutilisé par plusieurs pièces fixes 
+     * Ce bout de code est réutilisé par plusieurs pièces fixes
+     * dont la fonction ne réside pas tant dans le mouvement du 
+     * personage mais le changement d'etat du personnage (exemple
+     * l'Outil, la RoadMap ou la Glue) 
      */
     public void moveHunter(Hunter h){
         h.getGrid().removeLast(h.getGrid().getPos(h));
